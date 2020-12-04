af4b6effdf350c8222b529110e690fd3bd44671e
Jesswin Chetnani <64923931+jesswin@users.noreply.github.com>
2020-12-04 23:23:34 +0530

    done basic app

---
+++ app.js
@@ -0,0 +1,29 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-Input");
+var outputDiv = document.querySelector("#output");
+//var ServerURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=";
+var ServerURL="https://api.funtranslations.com/translate/minion.json?text=";
+
+btnTranslate.addEventListener("click", clickFunc);
+
+function constructUrl(text) {
+      console.log(ServerURL + text);
+      return ServerURL + text;
+      
+}
+
+function errorHandler(error) {
+      console.log(error, "error occured");
+      alert("An error Occured , Please Try after Sometime!");
+}
+
+function clickFunc() {
+      var inputText = txtInput.value;
+      fetch(constructUrl(inputText)).then(response => response.json()).then(json => {
+            var textTranslated = json.contents.translated;
+            outputDiv.innerText = textTranslated;
+      }).catch(errorHandler);
+      // we can access translated using [''] like maps
+      // outputDiv.innerText="anannana "+ txtInput.value;
+      // console.log(outputDiv.innerHTML);
+}
