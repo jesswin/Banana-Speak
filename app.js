23c602831e1f51171ad3ebea7d7e7c6b77850dd5
Jesswin Chetnani <64923931+jesswin@users.noreply.github.com>
2020-12-09 18:21:09 +0530

    fix

--- app.js
+++ app.js
@@ -27,3 +27,8 @@ function clickFunc() {
       // outputDiv.innerText="anannana "+ txtInput.value;
       // console.log(outputDiv.innerHTML);
 }
+document.querySelector("body").addEventListener("resize",resizee());
+function resizee(){
+      var innerScreenHeight = window.innerHeight;
+      document.querySelector(".footer").style.top = innerScreenHeight + "px";
+}
